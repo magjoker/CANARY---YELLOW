d239e69f0cd0ea2fb67b05d119106e834e77bd43
Jared Aragon <jaredaragon7@gmail.com>
2023-06-01 05:02:13 -0600

    gets the basic idea of the app implemented

---
+++ models/index.js
@@ -0,0 +1,25 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+// User.hasMany(Post, {
+//   foreignKey: 'user_id',
+//   onDelete: 'CASCADE'
+// });
+
+Post.belongsTo(User, {
+  foreignKey: 'userId',
+  onDelete: 'CASCADE'
+}); 
+
+Comment.belongsTo(User, {
+  foreignKey: 'userId',
+  onDelete: 'CASCADE'
+}); 
+
+Post.hasMany(Comment, {
+  foreignKey: 'postId',
+  onDelete: 'CASCADE'
+}); 
+
+module.exports = { User, Post, Comment };
---
+++ seeds/seed.js
@@ -0,0 +1,36 @@
+const sequelize = require('../config/connection');
+const { User, Post, Comment } = require('../models');
+
+const userData = require('./userData.json');
+const postData = require('./postData.json');
+const commentData = require('./commentData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  let posts = [1, 2, 3];
+
+  for (const post of postData) {
+    await Post.create({
+      ...post,
+      userId: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+  for (const comment of commentData) {
+    await Comment.create({
+      ...comment,
+      userId: users[Math.floor(Math.random() * users.length)].id,
+      postId: posts[Math.floor(Math.random() * posts.length)],
+    });
+  }
+
+  process.exit(0);
+};
+
+seedDatabase();
